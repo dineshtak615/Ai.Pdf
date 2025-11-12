f8f46d3fe18da5b41ead85ef2dbd7b134c26dc65
Dinesh tak <126274257+dineshtak615@users.noreply.github.com>
2025-11-12 11:42:07 +0530

    Fix some error

--- backend/app.py
+++ backend/app.py
@@ -275,7 +275,7 @@ import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 import google.generativeai as genai
